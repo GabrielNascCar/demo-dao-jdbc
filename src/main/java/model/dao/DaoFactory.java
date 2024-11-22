9c72eeabd2a066e588983d57ee339cdb9fb7423b
Gabriel do nascimento carvalho <nascimentocarvalho894@gmail.com>
2024-11-22 18:07:32 -0300

    findById implementation

--- src/main/java/model/dao/DaoFactory.java
+++ src/main/java/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SallerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
-        return new SallerDaoJDBC();
+        return new SallerDaoJDBC(DB.getConnection());
     }
 
 }
