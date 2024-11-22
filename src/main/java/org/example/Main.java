e55f73387446c1e1b14f23cf60ff568d58e31621
Gabriel do nascimento carvalho <nascimentocarvalho894@gmail.com>
2024-11-22 15:07:56 -0300

    Seller entity class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,10 @@ package org.example;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -12,7 +14,9 @@ public class Main {
 
         Department obj = new Department(1, "Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
 
     }
 }
